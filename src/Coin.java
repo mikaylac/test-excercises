cf590f203047af19d5a78c6f84a55a871a7047c9
pbaile64 <pbaile64@ford.com>
2018-12-31 11:35:09 -0500

    Created a coin enum. Created the CoinSlot and the CoinSlot test.

---
+++ src/Coin.java
@@ -0,0 +1,31 @@
+public enum Coin {
+
+    NICKEL(5.00, 21.21, .05),
+    DIME(2.27, 17.91, .10),
+    QUARTER(5.67, 24.26, .25);
+
+
+    private double weightInGrams;
+    private double diameterInMillimeters;
+    private double valueInCents;
+
+    private Coin(double weightInGrams, double diameterInMillimeters, double valueInCents){
+        this.weightInGrams = weightInGrams;
+        this.diameterInMillimeters = diameterInMillimeters;
+        this.valueInCents = valueInCents;
+    }
+
+    public double getWeightInGrams(){
+        return this.weightInGrams;
+    }
+
+    public double getDiameterInMillimeters(){
+        return this.diameterInMillimeters;
+    }
+
+    public double getValueInCents(){
+        return this.valueInCents;
+    }
+
+
+}
---
+++ src/CoinSlotTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class CoinSlotTest {
+
+    @Test
+    public void shouldAddAnyCoinValueToTotal(){
+        CoinSlot coinSlot = new CoinSlot();
+
+    }
+}
